4d00c66892faf61bccf73d29791247f4ffb65797
Aleksandra Tkachuk <tae27037@gmail.com>
2026-02-05 15:51:23 +0100

    hw

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import "./task-1.ts";
+import "./task-2.ts";
+import "./task-3.ts";
+import "./task-4.ts";
+import "./task-5.ts";
+import "./task-6.ts";
+import "./task-7.ts";
+import "./task-8.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
